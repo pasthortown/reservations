dcd3de2bfcdc51de9e2d5919011317f8c43a64cb
Luis Alfonso Salazar Vaca <luis.salazar@kfc.com.ec>
2024-08-22 17:12:11 -0500

    cambios

--- admin/src/app/views/catalogos/zonas/zonas.component.ts
+++ admin/src/app/views/catalogos/zonas/zonas.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  ButtonCloseDirective,
+  ButtonDirective,
+  ColComponent,
+  RowComponent,
+  ModalBodyComponent,
+  ModalComponent,
+  ModalFooterComponent,
+  ModalHeaderComponent,
+  ModalTitleDirective,
+  ModalToggleDirective,
+  TextColorDirective,
+  ThemeDirective,
+  TableDirective,
+  TableColorDirective,
+  TableActiveDirective,
+  BorderDirective,
+  AlignDirective,
+  PaginationComponent,
+  PageItemComponent,
+  PageLinkDirective,
+  InputGroupComponent,
+  InputGroupTextDirective,
+  FormControlDirective,
+  FormLabelDirective
+} from '@coreui/angular';
+
+import { IconDirective, IconSetService } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-zonas',
   standalone: true,
-  imports: [],
+  providers: [IconSetService],
   templateUrl: './zonas.component.html',
-  styleUrl: './zonas.component.scss'
+  styleUrl: './zonas.component.scss',
+  imports: [ IconDirective, InputGroupComponent, InputGroupTextDirective, FormControlDirective, FormLabelDirective, PaginationComponent, PageItemComponent, PageLinkDirective, TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective, TextColorDirective, ThemeDirective, ButtonCloseDirective, ButtonDirective, ColComponent, RowComponent, ModalComponent, ModalHeaderComponent, ModalTitleDirective, ModalBodyComponent, ModalFooterComponent, ModalToggleDirective, RouterLink]
 })
 export class ZonasComponent {
 
