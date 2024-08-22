2f45c6dce152d2437cbb59c697ec27de8a3a68df
Ivana Gonzalez <ivgonzalez@krugerschool.edu.ec>
2024-08-22 15:43:52 -0500

    mejoras visuales

--- front/src/app/components/footer/footer.component.ts
+++ front/src/app/components/footer/footer.component.ts
@@ -52,38 +52,28 @@ export class FooterComponent {
       title: 'Airbnb-friendly apartments',
     },
   ];
-  popularItems: FormDefault[] = [
+
+  popularItems = [
     {
-      title: 'Canmore',
-      subTitle: 'Apartment rentals',
+      title: 'Sobre nosotros',
+      data: [
+        { key: 'aboutUs', value: 'Luxuria Stays es una empresa creada con el propósito se alquilar apartamentos para cortas y largas estancias, colaborando con diferentes propietarios de diferentes ciudades del mundo.' },
+      ],
     },
     {
-      title: 'Benalmadena',
-      subTitle: 'Apartment rentals',
+      title: 'Oficinas',
+      data: [
+        { key: 'street', value: 'Av. Amazonas y Corea' },
+        { key: 'city', value: 'Quito - Ecuador' },
+      ],
     },
     {
-      title: 'Marbella',
-      subTitle: 'House rentals',
-    },
-    {
-      title: 'Mijas',
-      subTitle: 'Vacation rentals',
-    },
-    {
-      title: 'Prescott',
-      subTitle: 'Apartment rentals',
-    },
-    {
-      title: 'Scootdale',
-      subTitle: 'Vacation rentals',
-    },
-    {
-      title: 'Jasper',
-      subTitle: 'Cabin rentals',
-    },
-    {
-      title: 'Tucson',
-      subTitle: 'Car rentals',
+      title: 'Contacto',
+      data: [
+        { key: 'phone', value: '+593 99 658 3107' },
+        { key: 'email', value: 'luis.salazar@kfc.com.ec' },
+        { key: 'schedule', value: 'De lunes a viernes de 9:30h a 19:30h' },
+      ],
     },
   ];
   supportItems: string[] = [
