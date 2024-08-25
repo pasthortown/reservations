600f78bbbba0074bf5a7395cc82c9fb4547d0081
Luis Alfonso Salazar Vaca <luis.salazar@kfc.com.ec>
2024-08-25 00:02:57 -0500

    edicion alojamiento

--- admin/src/app/views/alojamientos/alojamientos.component.ts
+++ admin/src/app/views/alojamientos/alojamientos.component.ts
@@ -1,3 +1,4 @@
+import { MapComponent } from './../components/map/map.component';
 import { FileDropComponent } from './../components/file-drop/file-drop.component';
 import { StarsComponent } from './../components/stars/stars.component';
 import { FilesService } from 'src/app/services/file.service';
@@ -44,7 +45,8 @@ import {
   CardLinkDirective,
   CardSubtitleDirective,
   CardTextDirective,
-  CardTitleDirective
+  CardTitleDirective,
+  BadgeModule
 } from '@coreui/angular';
 
 import { IconDirective } from '@coreui/icons-angular';
@@ -57,23 +59,28 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './alojamientos.component.scss',
   providers:[CatalogService, FilesService],
   imports: [
-    FileDropComponent, StarsComponent, CardBodyComponent, CardComponent, CardFooterComponent, CardGroupComponent, CardHeaderComponent, CardImgDirective, CardLinkDirective, CardSubtitleDirective, CardTextDirective, CardTitleDirective, FormFloatingDirective, FormDirective, FormSelectDirective, FormsModule, ButtonGroupComponent, ButtonToolbarComponent, IconDirective, HttpClientModule, InputGroupComponent, InputGroupTextDirective, FormControlDirective, FormLabelDirective, PaginationComponent, PageItemComponent, PageLinkDirective, TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective, TextColorDirective, ThemeDirective, ButtonCloseDirective, ButtonDirective, ColComponent, RowComponent, ModalComponent, ModalHeaderComponent, ModalTitleDirective, ModalBodyComponent, ModalFooterComponent, ModalToggleDirective, RouterLink]
+    BadgeModule, MapComponent, FileDropComponent, StarsComponent, CardBodyComponent, CardComponent, CardFooterComponent, CardGroupComponent, CardHeaderComponent, CardImgDirective, CardLinkDirective, CardSubtitleDirective, CardTextDirective, CardTitleDirective, FormFloatingDirective, FormDirective, FormSelectDirective, FormsModule, ButtonGroupComponent, ButtonToolbarComponent, IconDirective, HttpClientModule, InputGroupComponent, InputGroupTextDirective, FormControlDirective, FormLabelDirective, PaginationComponent, PageItemComponent, PageLinkDirective, TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective, TextColorDirective, ThemeDirective, ButtonCloseDirective, ButtonDirective, ColComponent, RowComponent, ModalComponent, ModalHeaderComponent, ModalTitleDirective, ModalBodyComponent, ModalFooterComponent, ModalToggleDirective, RouterLink]
 })
 export class AlojamientosComponent implements OnInit{
   filter = '';
+  portada: any[] = [];
+
+  servicios: any[] = [];
+  condiciones: any[] = [];
+  selected_service: any = null;
+  selected_condition: any = null;
+
   alojamientos: any[] = [];
   alojamientos_shown: any[] = [];
-  alojamiento_selected = {
+  alojamiento_selected: any = {
     nombre: '',
-    personas: '',
+    personas: 0,
     metros: 0,
     habitaciones: 0,
     banos: 0,
     desde_noche: 0,
     desde_mes: 0,
     descripcion: '',
-    latitude: 0,
-    longitude: 0,
     ubication: {
         lat: 0,
         lng: 0
@@ -87,7 +94,7 @@ export class AlojamientosComponent implements OnInit{
     portada: null,
     images: [],
     rate: 0,
-    comments: []
+    comentarios: []
   };
   public visible = false;
   is_new = false;
@@ -99,18 +106,18 @@ export class AlojamientosComponent implements OnInit{
   }
 
   get_catalog() {
+    this.servicios = [];
+    this.condiciones=[];
     this.alojamientos = [];
     this.alojamiento_selected = {
       nombre: '',
-      personas: '',
+      descripcion: '',
+      personas: 0,
       metros: 0,
       habitaciones: 0,
       banos: 0,
       desde_noche: 0,
       desde_mes: 0,
-      descripcion: '',
-      latitude: 0,
-      longitude: 0,
       ubication: {
           lat: 0,
           lng: 0
@@ -124,7 +131,7 @@ export class AlojamientosComponent implements OnInit{
       portada: null,
       images: [],
       rate: 0,
-      comments: []
+      comentarios: []
     };
     let output_model = {
       nombre: true,
@@ -135,8 +142,6 @@ export class AlojamientosComponent implements OnInit{
       desde_noche: true,
       desde_mes: true,
       descripcion: true,
-      latitude: true,
-      longitude: true,
       ubication: true,
       check_in: true,
       check_out: true,
@@ -145,8 +150,14 @@ export class AlojamientosComponent implements OnInit{
       condiciones: true,
       servicios: true,
       rate: true,
-      comments: true
+      comentarios: true
     }
+    this.catalogService.get_items('servicios', { name: true, ico: true, description: true }).then( r => {
+      this.servicios = r.response;
+    }).catch( e => console.log(e) );
+    this.catalogService.get_items('condiciones', { name: true, ico: true, description: true  }).then( r => {
+      this.condiciones = r.response;
+    }).catch( e => console.log(e) );
     this.catalogService.get_items('alojamientos', output_model).then( r => {
       this.alojamientos = r.response;
       this.alojamientos.forEach((alojamiento: any) => {
@@ -194,5 +205,71 @@ export class AlojamientosComponent implements OnInit{
       this.fileService.delete_file('fotografias_alojamientos', item.image_id);
       this.get_catalog();
     }).catch( e => console.log(e) );
+  }
+
+  cargar_portada(event: any) {
+    if (event.validated) {
+      this.portada = event.files;
+    }
+  }
+
+  do_select_condition(condition_id: string) {
+    this.condiciones.forEach((condition: any) => {
+      if (condition.item_id == condition_id) {
+        this.selected_condition = condition;
+      }
+    });
+  }
+
+  do_select_service(service_id: string) {
+    this.servicios.forEach((service: any) => {
+      if (service.item_id == service_id) {
+        this.selected_service = service;
+      }
+    });
+  }
+
+  add_condition(condition: any) {
+    if (this.selected_condition == null) {
+      return;
+    }
+    if (!this.alojamiento_selected.condiciones) {
+      this.alojamiento_selected.condiciones = [];
+    }
+    let existe = false;
+    this.alojamiento_selected.condiciones.forEach((element: any)=> {
+      if (element.item_id == condition.item_id) {
+        existe = true;
+      }
+    });
+    if (!existe) {
+      this.alojamiento_selected.condiciones.push(condition);
+    }
+  }
+
+  add_service(service: any) {
+    if (this.selected_service == null) {
+      return;
+    }
+    if (!this.alojamiento_selected.servicios) {
+      this.alojamiento_selected.servicios = [];
+    }
+    let existe = false;
+    this.alojamiento_selected.servicios.forEach((element: any)=> {
+      if (element.item_id == service.item_id) {
+        existe = true;
+      }
+    });
+    if (!existe) {
+      this.alojamiento_selected.servicios.push(service);
+    }
+  }
+
+  delete_service(service_id: string) {
+    this.alojamiento_selected.servicios = this.alojamiento_selected.servicios.filter((servicio: any) => servicio.item_id !== service_id);
+  }
+
+  delete_condition(condition_id: string) {
+    this.alojamiento_selected.condiciones = this.alojamiento_selected.condiciones.filter((condicion: any) => condicion.item_id !== condition_id);
   }
 }
