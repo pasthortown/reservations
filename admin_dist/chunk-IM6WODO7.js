0be98e52acbe5be66a93006f6450389f40c324f4
Luis Alfonso Salazar Vaca <luis.salazar@kfc.com.ec>
2024-08-25 22:07:31 -0500

    admin dist

---
+++ admin_dist/chunk-IM6WODO7.js
@@ -0,0 +1,1 @@
+import"./chunk-IGH4DTA6.js";var t=[{path:"",loadComponent:()=>import("./chunk-RNCGKROX.js").then(o=>o.DashboardComponent),data:{title:$localize`Reportes`}}];export{t as routes};
---
+++ admin_dist/chunk-UAOFOE2L.js
@@ -0,0 +1,1 @@
+var t={api_auth:"http://localhost:5070/",api_catalog:"http://localhost:5071/",api_file:"http://localhost:5072/"};export{t as a};
